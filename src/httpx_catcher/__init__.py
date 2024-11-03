612f85ba933025e9bf97477419142da03a9bfc4a
ilotoki0804 <ilotoki0804@gmail.com>
2024-11-04 02:12:27 +0900

    Add required dunder

--- src/httpx_catcher/__init__.py
+++ src/httpx_catcher/__init__.py
@@ -1,1 +1,10 @@
-from ._catcher import AsyncCatcherTransport, init_transport, install
+from ._catcher import AsyncCatcherTransport, ModeType, init_transport, install, install_httpc
+
+__version__ = "0.1.0"
+__all__ = [
+    "AsyncCatcherTransport",
+    "ModeType",
+    "init_transport",
+    "install",
+    "install_httpc",
+]
